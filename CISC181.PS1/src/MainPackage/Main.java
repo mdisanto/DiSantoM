daf67ae05695451c9c7941968be2b8f50f349dff
micha_000 <micha_000@128.175.185.180>
2015-09-09 13:21:25 -0400

    Added DiSantoM

--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 			
-		GibbonsB.HelloWorld();
+		/*
+		 * My github account name: mdisanto
+		 */
+		DiSantoM.HelloWorld();
 	}
 
 }
